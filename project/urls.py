2672b49f37ee270abf92c6305f1047aba2a78914
shaheem-pp <shanofficial2000@gmail.com>
2024-12-10 21:14:44 -0500

    urls and views

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
+from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("appRecipe.urls")),
+    path("auth", include("appAuth.urls")),
+    path("user", include("appUser.urls")),
+
 ]
 
 # Serve static and media files during development
